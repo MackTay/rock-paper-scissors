abe55cff452f993787aeba63aab42159f4e50456
MackTay <191326787+MackTay@users.noreply.github.com>
2025-02-27 11:31:23 +0900

    Add function that allows user to play 1 round
    Need the ability to compare human and computer choices. Add function with if/else statement that compares choices and returns a string depending on the result. Increment the player/computer score depending on result.

--- rps.js
+++ rps.js
@@ -4,11 +4,11 @@
 function getComputerChoice() {
     let answer = Math.random();
     if (answer <= 0.333) {
-        return "Rock";
+        return "rock";
     } else if (answer > 0.333 && answer <= 0.666) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -17,5 +17,24 @@ function getHumanChoice() {
     return prompt("Rock, paper, or scissors?");
 }
 
+// Create variables that will track human and computer scores
 let humanScore = 0;
 let computerScore = 0;
+
+// Create a function that allows user to play a single round
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! Try again.");
+        getHumanChoice();
+    } else if
+    ((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+        humanScore += 1;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore += 1;
+    }
+}
